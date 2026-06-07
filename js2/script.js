3ca462ef6dd07004d0504267831087eade27a055
Aditya <adityac0104@gmail.com>
2026-06-07 14:14:09 +0530

    first commit - all js projects

---
+++ js2/script.js
@@ -0,0 +1,27 @@
+console.log("this")
+
+let a = "aditya";
+console.log(a)
+console.log(a[0])
+console.log(a[1])
+
+console.log(a.length)
+
+let b = "aadityaaaa"
+let friend = "Rohan"
+
+console.log(b.toLowerCase())
+console.log(b.toUpperCase())
+console.log(b.slice(1,4))
+console.log(b.slice(1))
+console.log(b.replace("aa","67"))
+console.log(b.concat(friend))
+console.log(b.concat("hello","hiii"))
+
+console.log(b.charAt(2))
+console.log(b.indexOf("ty"))
+
+console.log(`my name is ${b} and my friend is ${friend}`)
+console.log("My name is "+b+ " and my friend name is "+friend)
+
+
